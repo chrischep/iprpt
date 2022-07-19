87c42bf439b6d86177ce2ba83bafc73b8f31e9ee
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-19 23:13:14 +0300

    get form input

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+const div = document.querySelector('.dataContainer'),
+shedA = document.getElementById('shedA'),
+shedB = document.getElementById('shedB'),
+shedC  = document.getElementById('shedC'),
+shedD = document.getElementById('shedD')
