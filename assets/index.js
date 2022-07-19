d1591afae62fe0813d9732ecdf1f9a5ae4ab62c3
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-19 23:18:27 +0300

    calculate shed totals

--- assets/index.js
+++ assets/index.js
@@ -3,3 +3,15 @@ shedA = document.getElementById('shedA'),
 shedB = document.getElementById('shedB'),
 shedC  = document.getElementById('shedC'),
 shedD = document.getElementById('shedD')
+
+function generateData(){
+    document.getElementById('data').className = ''
+    let totalMilkInLitres = parseInt(shedA.value) + parseInt(shedB.value) + parseInt(shedC.value) + parseInt(shedD.value)
+    console.log(totalMilkInLitres)
+    const p1 = document.createElement('p'),
+    p2 = document.createElement('p'),
+    p3 = document.createElement('p'),
+    p4 = document.createElement('p'),
+    p5 = document.createElement('p')
+    p5.className = 'bolder'
+}
