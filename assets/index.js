b576d6d77ba50dc9ef6cc31f610b41aff923543c
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-19 23:23:16 +0300

    insert  child elements to data container

--- assets/index.js
+++ assets/index.js
@@ -5,9 +5,11 @@ shedC  = document.getElementById('shedC'),
 shedD = document.getElementById('shedD')
 
 function generateData(){
+    //calculate total milk in ltrs
     document.getElementById('data').className = ''
     let totalMilkInLitres = parseInt(shedA.value) + parseInt(shedB.value) + parseInt(shedC.value) + parseInt(shedD.value)
     console.log(totalMilkInLitres)
+    //create elements for displaying data
     const p1 = document.createElement('p'),
     p2 = document.createElement('p'),
     p3 = document.createElement('p'),
