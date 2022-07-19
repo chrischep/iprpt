d44bcc0f0ff8f85f9753644a3cfe72d80b07d1df
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-19 23:29:50 +0300

    calculate monthly income

--- assets/index.js
+++ assets/index.js
@@ -51,3 +51,38 @@ function calculateDailyWeeklyIncome(litres){
         //Append the elements to the div container
         document.getElementById('dataContainer').append(h3,daily, weekly)
 }
+function calculateMonthlyIncome(litres){
+    //create elements for displaying monthly incomes
+    const jan = document.createElement('p'),
+    feb = document.createElement('p'),
+    mar = document.createElement('p'),
+    apr = document.createElement('p'),
+    may = document.createElement('p'),
+    jun = document.createElement('p'),
+    jul = document.createElement('p'),
+    aug = document.createElement('p'),
+    sep = document.createElement('p'),
+    oct = document.createElement('p'),
+    nov = document.createElement('p'),
+    dec = document.createElement('p'),
+    h3 = document.createElement('h3')
+    h3.textContent=("Monthly Income")
+
+
+    jan.textContent = `The total January income per month is ${litres*31} Kenyan Shillings`
+    feb.textContent = `The total February income per month is ${litres*29} Kenyan Shillings`
+    mar.textContent = `The total March income per month is ${litres*31} Kenyan Shillings`
+    apr.textContent = `The total April income per month is ${litres*30} Kenyan Shillings`
+    may.textContent = `The total May income per month is ${litres*31} Kenyan Shillings`
+    jun.textContent = `The total June income per month is ${litres*30} Kenyan Shillings`
+    jul.textContent = `The total July income per month is ${litres*31} Kenyan Shillings`
+    aug.textContent = `The total August income per month is ${litres*31} Kenyan Shillings`
+    sep.textContent = `The total September income per month is ${litres*30} Kenyan Shillings`
+    oct.textContent = `The total October income per month is ${litres*31} Kenyan Shillings`
+    nov.textContent = `The total November income per month is ${litres*30} Kenyan Shillings`
+    dec.textContent = `The total December income per month is ${litres*31} Kenyan Shillings`
+
+
+    document.getElementById('dataContainer').append(h3, jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+
+}
