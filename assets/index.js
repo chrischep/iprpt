717b194c5d4e7f2f4cd61ee8620afa946c278bb8
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-19 23:24:00 +0300

    comments

--- assets/index.js
+++ assets/index.js
@@ -16,4 +16,14 @@ function generateData(){
     p4 = document.createElement('p'),
     p5 = document.createElement('p')
     p5.className = 'bolder'
+
+    //Display daily milk production in litres
+    p1.textContent = `The daily total milk production pin shed A is ${shedA.value} litres`
+    p2.textContent = `The daily total milk production in shed B is ${shedB.value} litres`
+    p3.textContent = `The daily total milk production in shed C is ${shedC.value} litres`
+    p4.textContent = `The daily total milk production in shed D is ${shedD.value} litres`
+    p5.textContent = `The total milk produced per day is ${totalMilkInLitres} litres`
+    
+    //insert the elements to the div container
+    document.getElementById('dataContainer').append(p1,p2,p3,p4,p5)
 }
